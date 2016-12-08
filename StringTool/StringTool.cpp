650aeb68c9a5891d0711e9ce58a5189e46869087
sandrei42 <saupeandrei@yahoo.com>
2016-12-08 02:23:15 +0200

    Added hard coded boost library to StringTool project
    Implement first version for StringTool
    Update unit tests

--- StringTool/StringTool.cpp
+++ StringTool/StringTool.cpp
@@ -2,8 +2,44 @@
 
 #include <iostream>
 
+#include <boost/program_options.hpp>
+
 int main(int argc, char** argv)
 {
-	std::cout << "Hamming distance: " << HammingDist::hammingDistance( "a" , "b" ) << std::endl;
+	namespace po = boost::program_options;
+
+	std::string first;
+	std::string second;
+	try
+	{
+		po::options_description desc("Allowed options");
+		desc.add_options()
+			("help", "produce help message")
+			("first,f", po::value<std::string>(&first)->required(), "first string to compare to")
+			("second,s", po::value<std::string>(&second)->required(), "second string to compare to")
+			;
+
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		po::notify(vm);
+
+		if (vm.count("help"))
+		{
+			std::cout << desc << std::endl;
+			return 0;
+		}
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+		return 1;
+	}
+	catch (...)
+	{
+		std::cout << "Unknown error!" << std::endl;
+		return 1;
+	}
+
+	std::cout << "Hamming distance: " << HammingDist::hammingDistance( first , second ) << std::endl;
 	return 0;
 }
--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -3,18 +3,99 @@
 
 #include <HammingDistanceLib.h>
 
-TEST_CASE("Hamming distance is 42", "[basic]")
+#include <random>
+#include <functional>
+
+TEST_CASE("Hamming distance", "[basic]")
 {
-	HammingDist::blob firstBlob = { 42 };
-	HammingDist::blob secondBlob = { 42 };
-	REQUIRE( HammingDist::hammingDistance(firstBlob, secondBlob) == 0 );
+	SECTION("one value simple test")
+	{
+		HammingDist::blob firstBlob = { 42 };
+		HammingDist::blob secondBlob = { 42 };
+		REQUIRE(HammingDist::hammingDistance(firstBlob, secondBlob) == 0);
+		REQUIRE(HammingDist::hammingDistance(secondBlob, firstBlob) == 0);
+		secondBlob = { 43 };
+		REQUIRE(HammingDist::hammingDistance(firstBlob, secondBlob) != 0);
+		REQUIRE(HammingDist::hammingDistance(secondBlob, firstBlob) != 0);
+	}
+
+	SECTION("One value, cheking bits")
+	{
+		HammingDist::blob firstBlob = { 0 };
+		char val = 0;
+		for (size_t i = 1; i <= CHAR_BIT; ++i)
+		{
+			val <<= 1;
+			val |= 1;
+			HammingDist::blob secondBlob = { val };
+			REQUIRE(HammingDist::hammingDistance(firstBlob, secondBlob) == i);
+		}
+	}
+
+	SECTION("One value, cheking bits, rotate one")
+	{
+		HammingDist::blob firstBlob = { 0 };
+		char val = 1;
+		for (size_t i = 1; i <= CHAR_BIT; ++i)
+		{
+			HammingDist::blob secondBlob = { val };
+			REQUIRE(HammingDist::hammingDistance(firstBlob, secondBlob) == 1);
+			val <<= 1;
+		}
+	}
 }
+
+TEST_CASE("Hamming distance random", "[random]")
+{
+	std::default_random_engine generator;
+	std::uniform_int_distribution<int> distribution(0, 255);
+	auto dice = std::bind(distribution, generator);
+
+	size_t elements = dice();
+
+	std::vector<HammingDist::blobType> tempData;
+	for (size_t elem = 0; elem < elements; ++elem)
+	{
+		tempData.push_back(dice());
+	}
+	HammingDist::blob first(&tempData[0], elements);
+
+	tempData.clear();
+	for (size_t elem = 0; elem < elements; ++elem)
+	{
+		tempData.push_back(dice());
+	}
+	HammingDist::blob second(&tempData[0], elements);
+
+	tempData.clear();
+	for (size_t elem = 0; elem < elements; ++elem)
+	{
+		tempData.push_back(dice());
+	}
+	HammingDist::blob third(&tempData[0], elements);
+
+	SECTION("Distance properties")
+	{
+		REQUIRE(HammingDist::hammingDistance(first, second) >= 0);
+		REQUIRE(HammingDist::hammingDistance(first, second) == HammingDist::hammingDistance(second, first));
+		REQUIRE(HammingDist::hammingDistance(first, second) <= HammingDist::hammingDistance(first, second)
+															 + HammingDist::hammingDistance(second, third));
+		if (HammingDist::hammingDistance(first, second) == 0)
+		{
+			REQUIRE((first - second).sum() == 0);
+		}
+	}
+}
+
+//ToDo Negative testing
 
 TEST_CASE("Hamming distance grouping", "[basic]")
 {
-	HammingDist::blob firstBlob = { 3 , 21 };
+	HammingDist::blob firstBlob = { 1 , 21 };
 	HammingDist::blob secondBlob = { 0 , 21 };
 	std::vector<HammingDist::distValue> output = HammingDist::hammingDistance(firstBlob, secondBlob, 1);
-	REQUIRE(output[0] == 0);
+	REQUIRE(output[0] == 1);
 	REQUIRE(output[1] == 0);
+	output = HammingDist::hammingDistance(firstBlob, secondBlob, 2);
+	REQUIRE(output[0] == 1);
 }
